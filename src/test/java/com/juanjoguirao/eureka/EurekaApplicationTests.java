1cee38965d283c573cb880bfbdbcfc55fbccdc9f
Juanjo Guirao <juan.jose.guirao@gmail.com>
2020-01-12 08:03:57 +0000

    Initial Commit

---
+++ src/test/java/com/juanjoguirao/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanjoguirao.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
